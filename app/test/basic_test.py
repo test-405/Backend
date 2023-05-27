60c3e47572891197e721370856e36fa3e5fcf7a9
LernerW <1121430447@qq.com>
2023-05-28 01:00:15 +0800

    [test]add github action test config

--- app/test/basic_test.py
+++ app/test/basic_test.py
@@ -5,15 +5,19 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 print(sys.path)
 
-from app.app import app, db
 import unittest
 
+DB_USER = os.getenv("DB_USER", default="root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", default="")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", default="paper_test")
 mysql = {
-    'user': 'root',
-    'passwd': '',
+    'user': DB_USER,
+    'passwd': DB_PASSWORD,
     'host': 'localhost',
-    'db': 'paper_test'
+    'db': MYSQL_DATABASE
 }
+print(sys.path)
+print(mysql)
 
 username = "haoaho"
 password = "123456"
@@ -23,11 +27,16 @@ mysqlConfig = "mysql+pymysql://{}:{}@{}/{}?charset=utf8mb4" \
 
 class BasicTest(unittest.TestCase):
     def setUp(self):
+        from app.app import app
         with app.app_context():
             app.config["TESTING"] = True
             app.config["DEBUG"] = False
             app.config["SQLALCHEMY_DATABASE_URI"] = mysqlConfig
             self.app = app.test_client()
+            from app.app import db
+            print("+++++++++")
+            print(mysqlConfig)
+            print("+++++++++")
             db.drop_all()
             db.create_all()
             self.assertEqual(app.debug, False)
