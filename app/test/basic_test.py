76f519821f8585ee47607167331dd337d692dc4a
LernerW <1121430447@qq.com>
2023-05-28 23:57:26 +0800

    [test]detach util class and test class

--- app/test/basic_test.py
+++ app/test/basic_test.py
@@ -8,15 +8,17 @@ print(sys.path)
 from app.app import app, db
 import unittest
 
-class BasicTest(unittest.TestCase):
-    def setUp(self):
+class BasicUtil():
+    def __init__(self):
         with app.app_context():
             app.config["TESTING"] = True
             app.config["DEBUG"] = False
             self.app = app.test_client()
             db.drop_all()
             db.create_all()
-            self.assertEqual(app.debug, False)
+
+class BasicTest(unittest.TestCase):
+    pass
 
 
 if __name__ == "__main__":
