a0650c58af62282aea03626524429a05376d26c3
LernerW <1121430447@qq.com>
2023-05-28 13:03:07 +0800

    [refactor]introduce config loading and app init

--- app/config.py
+++ app/config.py
@@ -1,9 +1,39 @@
-mysql = {
-    'user': 'root',
-    'passwd': '790713wh.',
-    'host': 'localhost',
-    'db': 'paper'
-}
+import os
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+##################
+# MYSQL SETTINGS #
+##################
+MYSQL_USER = os.getenv("DB_USER", default="root")
+MYSQL_PASSWORD = os.getenv("DB_PASSWORD", default="")
+MYSQL_HOST = os.getenv("MYSQL_HOST", default="localhost")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", default="paper_test")
+
+##################
+# JWT SETTINGS   #
+##################
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", default="super-secret")
+
+##################
+# USER CONFIG    #
+##################
+GLOBAL_CONFIG_JSON = os.getenv("GLOBAL_CONFIG_JSON", default="")
+GLOBAL_CONFIG = {}
+if GLOBAL_CONFIG_JSON != "":
+    with open(GLOBAL_CONFIG_JSON, "r") as f:
+        config = json.load(f)
+
+####################
+# CONFIG READ OVER #
+####################
 
 mysqlConfig = "mysql+pymysql://{}:{}@{}/{}?charset=utf8mb4" \
-    .format(mysql['user'], mysql['passwd'], mysql['host'], mysql['db'])
+    .format(MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_DATABASE)
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = mysqlConfig
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PROPAGATE_EXCEPTIONS = True
+    JWT_SECRET_KEY = JWT_SECRET_KEY
---
+++ app/initializers/mysql.py
@@ -0,0 +1,7 @@
+from db import db
+
+def init_db(app=None):
+    db.init_app(app)
+    with app.app_context():
+        import models
+        db.create_all()
